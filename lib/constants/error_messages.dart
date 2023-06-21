452d0162e70f2d0a7b2384adf164a87c0de17405
Yuki <82790806+Taaaa-yuki@users.noreply.github.com>
2023-06-21 22:43:00 +0900

    Refactor ErrorMessages class to use inheritance

--- lib/constants/error_messages.dart
+++ lib/constants/error_messages.dart
@@ -1,10 +1,10 @@
 import 'package:lyrics_app/constants/message.dart';
 
 class ErrorMessages extends Message {
-  static final failedToGetLyrics = Message('Failed to get lyrics. \nPlease try again.');
-  static final urlEmpty = Message('Please enter a URL');
-  static final enterLyricsUrlAndPressButton = Message('Enter a lyrics URL and press the button to get lyrics.');
-  static final albumEmpty = Message('Please enter an album information');
+  static final failedToGetLyrics = ErrorMessages('Failed to get lyrics. \nPlease try again.');
+  static final urlEmpty = ErrorMessages('Please enter a URL');
+  static final enterLyricsUrlAndPressButton = ErrorMessages('Enter a lyrics URL and press the button to get lyrics.');
+  static final albumEmpty = ErrorMessages('Please enter an album information');
 
   ErrorMessages(String text) : super(text);
 }
